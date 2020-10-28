4ed2e2751f5dec11e66c2d72f431e63fdb45d801
Shunen1412 <63414222+Shunen1412@users.noreply.github.com>
2020-10-28 13:54:24 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  TOKEN: "[token caché]",
+  DBCONNECTION: "mongodb+srv://DragonProtectAdmin:Dr@gonqui1@dragonprotect.5w3ns.mongodb.net/dragonprotect?retryWrites=true&w=majority",
+  DEFAULTSETTINGS: {
+    prefix: "dp.",
+    logChannel: "#🔗▕-logs-staff",
+    welcomeMessage: "Welcome ${user}"
+  }
+}
